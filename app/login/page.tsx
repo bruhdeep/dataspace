1d76e6ff275f3925f7248328099a8901490398e1
Pradip Shrestha <shresthapradeep47@gmail.com>
2024-01-24 13:30:38 +0545

    image fix

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,14 +2,14 @@ import Link from "next/link";
 
 export default function Login() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-[url('https://i.ibb.co/0FZv7QY/BgImage.png')] bg-cover">
+    <div className="flex items-center justify-center min-h-screen bg-[url('https://ewm.swiss/application/files/5716/0379/3279/social_media_.jpg')] bg-cover">
       <div className="w-full max-w-[90%] md:max-w-[60%] lg:max-w-[40%] xl:max-w-[30%] h-auto md:h-[80%] my-9 rounded-xl bg-white bg-opacity-75">
         <div className="w-[90%] md:w-[80%] lg:w-[90%] mx-auto h-full px-14 py-3 flex flex-col justify-between">
           <div className="h-[20%] my-10">
             <img
-              src="https://dataspace.com.np/storage/2023/08/cropped-cropped-dataspace-1-1-193x48.png"
+              src="/dataspacelogo.png"
               alt="dataspace"
-              className="m-auto w-[55%] pt-5"
+              className="m-auto w-[65%] pt-5"
             />
           </div>
           <div className="h-[10%] mb-5">
@@ -30,7 +30,6 @@ export default function Login() {
 
               <div>
                 <input
-                  type="password"
                   id="password"
                   name="password"
                   placeholder="Password"
@@ -48,7 +47,7 @@ export default function Login() {
 
               <button
                 type="submit"
-                className="w-full h-20 text-2xl font-semibold bg-[#0D5077] text-white px-4 py-2 rounded-2xl hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+                className="w-full h-20 text-2xl font-semibold bg-[#0D5077] text-white px-4 py-2 rounded-2xl hover:bg-blue-600 duration-200   focus:outline-none focus:bg-blue-600"
               >
                 Login
               </button>
